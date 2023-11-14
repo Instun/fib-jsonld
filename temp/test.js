8c5c1996997aebbb916686ce72c1982e453bb600
xicilion <lion@9465.net>
2023-11-14 22:44:11 +0800

    v1.0.1.

--- temp/test.js
+++ temp/test.js
@@ -1,37 +1,36 @@
 var jsonld = require('..');
-var ssl = require('ssl');
-
-ssl.loadRootCerts()
 
 const doc = {
     "http://schema.org/name": "Manu Sporny",
-    "http://schema.org/url": { "@id": "http://manu.sporny.org/" },
-    "http://schema.org/image": { "@id": "http://manu.sporny.org/images/manu.png" }
+    "http://schema.org/url": "http://manu.sporny.org/",
+    "http://schema.org/image": "http://manu.sporny.org/images/manu.png"
 };
 
 const context = {
     "name": "http://schema.org/name",
-    "homepage": { "@id": "http://schema.org/url", "@type": "@id" },
-    "image": { "@id": "http://schema.org/image", "@type": "@id" }
+    "homepage": "http://schema.org/url",
+    "image": "http://schema.org/image"
 };
 
 // compact a document according to a particular context
 const compacted = jsonld.compact(doc, context);
+console.log("================= compacted =================");
 console.dir(compacted, { depth: 4 });
 
 const expanded = jsonld.expand(compacted);
-
+console.log("================= expanded =================");
 console.dir(expanded, { depth: 4 });
 
 const flattened = jsonld.flatten(doc);
+console.log("================= flattened =================");
 console.dir(flattened, { depth: 4 });
 
 const canonized = jsonld.canonize(doc, {
     algorithm: 'URDNA2015',
     format: 'application/n-quads'
 });
-
+console.log("================= canonized =================");
 console.log(canonized);
 
-var r = jsonld.get('https://json-ld.org/contexts/person.jsonld');
-console.dir(r, { depth: 4 });
+// var r = jsonld.get('https://json-ld.org/contexts/person.jsonld');
+// console.dir(r, { depth: 4 });
