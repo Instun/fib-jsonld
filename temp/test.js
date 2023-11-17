6cfaeac7460caf172ad288b0fc213c0f503d2dae
xicilion <lion@9465.net>
2023-11-17 21:23:47 +0800

    v1.0.2.

--- temp/test.js
+++ temp/test.js
@@ -25,10 +25,48 @@ const flattened = jsonld.flatten(doc);
 console.log("================= flattened =================");
 console.dir(flattened, { depth: 4 });
 
-const canonized = jsonld.canonize(doc, {
-    algorithm: 'URDNA2015',
-    format: 'application/n-quads'
-});
+const canonized = jsonld.canonize({
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://w3id.org/security/suites/jws-2020/v1",
+    { "@vocab": "https://example.com/#" }
+  ],
+  "id": "https://example.com/credential/123456",
+  "type": ["VerifiableCredential", "VerifiableBusinessCard"],
+  "name": "Verifiable Business Card",
+  "relatedLink": [
+    {
+      "type": "LinkRole",
+      "target": "https://example.com/organizations/example-org/presentations/available",
+      "linkRelationship": "OrganizationPresentationEndpoint"
+    }
+  ],
+  "issuanceDate": "2016-12-31T23:59:59Z",
+  "expirationDate": "2038-01-19T03:14:08Z",
+  "issuer": {
+    "id": "did:example:123",
+    "type": "Organization",
+    "name": "Grady, Purdy and Pacocha",
+    "description": "Secured 24/7 neural-net",
+    "address": {
+      "type": "PostalAddress",
+      "streetAddress": "0516 Kendrick Heights",
+      "addressLocality": "Port Reba",
+      "addressRegion": "South Carolina",
+      "postalCode": "53062-7356",
+      "addressCountry": "Austria"
+    },
+    "email": "Cora31@example.net",
+    "phoneNumber": "555-158-2528",
+    "faxNumber": "555-577-3567"
+  },
+  "credentialSubject": {
+    "type": ["Organization"],
+    "name": "Steel Manufacturer Org",
+    "url": "https://www.example.com/"
+  }
+}
+);
 console.log("================= canonized =================");
 console.log(canonized);
 
